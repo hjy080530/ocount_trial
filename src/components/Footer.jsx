fd1256a3f5a0900db20be1fa61fe96962a950abb
hwangjiyeong916 <bomaye119@gmail.com>
2024-10-16 13:34:21 +0900

    test commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,13 @@ import react from 'react';
 const Footer = () => {
     return (
         <div className="footer">
-            <p className="footer-text">아리소리</p>
+            <p className="footer-text">
+                상호:부산소프트웨어 마이스터고등학고 사회적 협동조합 |대표:김민경(이사장)<br /> 
+                사업자 등록전호: 214-82-16238<br />
+                주소:부산광역시 강서구 가락대로 1393 부산소프트웨어 마이스터고등학교 융합관 공간-아리소리<br />
+                전화번호 : 051-970-1709
+            </p>
         </div>
     );
 };
-export default Footer;
+export default Footer;//ㄴㅊㅁ
